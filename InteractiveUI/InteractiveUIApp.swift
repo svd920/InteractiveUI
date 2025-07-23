d7c474d1595c2cad35dbe3fd0ea2cc38291a49eb
Satya Dindi <satyadindi1@gmail.com>
2025-07-24 02:10:25 +0530

    Initial Commit

---
+++ InteractiveUI/InteractiveUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveUIApp.swift
+//  InteractiveUI
+//
+//  Created by Satya Dindi on 7/24/25.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
